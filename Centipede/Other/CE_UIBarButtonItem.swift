662c45aa05df4f10a80e5a4013d19d34ee030079
柯磊 <kelei0017@gmail.com>
2015-06-08 21:40:53 +0800

    增加CE_UIBarButtonItem和CE_UIGestureRecognizer_Action

---
+++ Centipede/Other/CE_UIBarButtonItem.swift
@@ -0,0 +1,83 @@
+//
+//  CE_UIBarButtonItem.swift
+//  Centipede
+//
+//  Created by kelei on 15/6/8.
+//  Copyright (c) 2015年 kelei. All rights reserved.
+//
+
+import UIKit
+
+public typealias CE_UIBarButtonItemAction = (gestureRecognizer: UIBarButtonItem) -> Void
+
+public extension UIBarButtonItem {
+    
+    public func action(action: CE_UIBarButtonItemAction?) -> Self {
+        return on(action)
+    }
+    
+}
+
+// MARK: - Internal
+
+private struct Static { static var AssociationKey: UInt8 = 0 }
+
+private typealias UIBarButtonItemProxies = [String: UIBarButtonItemProxy]
+
+internal class UIBarButtonItemProxy : NSObject {
+    
+    var action: CE_UIBarButtonItemAction
+    
+    init(_ action: CE_UIBarButtonItemAction) {
+        self.action = action
+    }
+    
+    func act(gestureRecognizer: UIBarButtonItem) {
+        action(gestureRecognizer: gestureRecognizer)
+    }
+}
+
+internal extension UIBarButtonItem {
+    
+    private var proxies: UIBarButtonItemProxies {
+        get {
+            if let result = objc_getAssociatedObject(self, &Static.AssociationKey) as? UIBarButtonItemProxies {
+                return result
+            } else {
+                return setter(UIBarButtonItemProxies())
+            }
+        }
+        set {
+            setter(newValue)
+        }
+    }
+    
+    private func setter(newValue: UIBarButtonItemProxies) -> UIBarButtonItemProxies {
+        objc_setAssociatedObject(self, &Static.AssociationKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN));
+        return newValue
+    }
+    
+    internal func on(action: CE_UIBarButtonItemAction?) -> Self {
+        self.off()
+        
+        if action == nil {
+            return self
+        }
+        
+        let proxy = UIBarButtonItemProxy(action!)
+        self.target = proxy
+        self.action = "act:"
+        proxies[""] = proxy
+
+        return self
+    }
+    
+    internal func off() -> Self {
+        if let proxy = proxies[""] {
+            target = nil
+            action = ""
+            proxies.removeValueForKey("")
+        }
+        return self
+    }
+}
---
+++ Centipede/Other/CE_UIGestureRecognizer_Action.swift
@@ -0,0 +1,87 @@
+//
+//  CE_UIGestureRecognizer_Action.swift
+//  Centipede
+//
+//  Created by kelei on 15/6/8.
+//  Copyright (c) 2015年 kelei. All rights reserved.
+//
+
+import UIKit
+
+public typealias CE_UIGestureRecognizerAction = (gestureRecognizer: UIGestureRecognizer) -> Void
+
+public extension UIGestureRecognizer {
+    
+    public convenience init(action: CE_UIGestureRecognizerAction) {
+        let proxy = UIGestureRecognizerProxy(action)
+        self.init(target:proxy, action:"act:")
+        proxies[""] = proxy
+    }
+    
+    public func action(action: CE_UIGestureRecognizerAction?) -> Self {
+        return on(action)
+    }
+    
+}
+
+// MARK: - Internal
+
+private struct Static { static var AssociationKey: UInt8 = 0 }
+
+private typealias UIGestureRecognizerProxies = [String: UIGestureRecognizerProxy]
+
+internal class UIGestureRecognizerProxy : NSObject {
+    
+    var action: CE_UIGestureRecognizerAction
+    
+    init(_ action: CE_UIGestureRecognizerAction) {
+        self.action = action
+    }
+    
+    func act(gestureRecognizer: UIGestureRecognizer) {
+        action(gestureRecognizer: gestureRecognizer)
+    }
+}
+
+internal extension UIGestureRecognizer {
+    
+    private var proxies: UIGestureRecognizerProxies {
+        get {
+            if let result = objc_getAssociatedObject(self, &Static.AssociationKey) as? UIGestureRecognizerProxies {
+                return result
+            } else {
+                return setter(UIGestureRecognizerProxies())
+            }
+        }
+        set {
+            setter(newValue)
+        }
+    }
+    
+    private func setter(newValue: UIGestureRecognizerProxies) -> UIGestureRecognizerProxies {
+        objc_setAssociatedObject(self, &Static.AssociationKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN));
+        return newValue
+    }
+    
+    internal func on(action: CE_UIGestureRecognizerAction?) -> Self {
+        self.off()
+        
+        if action == nil {
+            return self
+        }
+        
+        let proxy = UIGestureRecognizerProxy(action!)
+        addTarget(proxy, action: "act:")
+        proxies[""] = proxy
+        
+        return self
+    }
+    
+    internal func off() -> Self {
+        if let proxy = proxies[""] {
+            self.removeTarget(proxy, action: "act:")
+            proxies.removeValueForKey("")
+        }
+        return self
+    }
+}
